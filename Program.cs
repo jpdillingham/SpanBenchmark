6dc900a2f83362ab8a9035727b9c63a93fd89420
JP Dillingham <jpdillingham@gmail.com>
2018-10-29 22:17:18 -0500

    add benchmark

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Text;
 
 namespace SpanBenchmark
 {
@@ -6,7 +8,53 @@ namespace SpanBenchmark
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (long i = 10000000; i < 10000000000000; i*= 10)
+            {
+                SpanLoop(i);
+                ArrayLoop(i);
+            }
+
+            Console.WriteLine($"Done.");
+
+            Console.ReadKey();
+        }
+
+        static void SpanLoop(long count)
+        {
+            var span = new ReadOnlySpan<byte>(Encoding.ASCII.GetBytes(new string(' ', 4096)));
+
+            Stopwatch s = new Stopwatch();
+            s.Start();
+
+            int pos = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                pos = (pos + 4) % span.Length - 4;
+                int x = BitConverter.ToInt32(span.Slice(pos, 4));
+            }
+
+            s.Stop();
+            Console.WriteLine($"Span \t{count}\t{s.ElapsedMilliseconds}ms");
+        }
+
+        static void ArrayLoop(long count)
+        {
+            var array = Encoding.ASCII.GetBytes(new string(' ', 4096));
+
+            Stopwatch s = new Stopwatch();
+            s.Start();
+
+            int pos = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                pos = (pos + 4) % array.Length - 4;
+                int x = BitConverter.ToInt32(array, pos);
+            }
+
+            s.Stop();
+            Console.WriteLine($"Array \t{count}\t{s.ElapsedMilliseconds}ms");
         }
     }
 }
