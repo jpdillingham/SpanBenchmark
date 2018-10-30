11fb3680258970b492f897c0edae5788b8068f3a
JP Dillingham <jpdillingham@gmail.com>
2018-10-29 21:32:36 -0500

    stub from 'dotnet new'

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpanBenchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
